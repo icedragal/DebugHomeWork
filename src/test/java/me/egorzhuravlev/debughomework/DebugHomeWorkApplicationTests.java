7f62d7d0a1a6e7793e9f2cd6d5f4bf47907d8bb9
Icedragal <icedragal@gmail.com>
2023-01-23 18:11:49 +0300

    Initial commit

---
+++ src/test/java/me/egorzhuravlev/debughomework/DebugHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package me.egorzhuravlev.debughomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugHomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
